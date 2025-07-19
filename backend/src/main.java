193667187da5693278b68ddec0f1c387cf74557d
sunegg-yo <sun.egl2025@gmail.com>
2025-07-19 23:49:01 +0900

    xml修正

---
+++ backend/src/main.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+    @PostConstruct
+    public void initFirebase() throws Exception {
+        FileInputStream serviceAccount = new FileInputStream("serviceAccountKey.json");
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://customer-app-62f73.firebaseio.com/")
+                .build();
+        FirebaseApp.initializeApp(options);
+    }
+
+    @PostMapping("/add")
+    public String addCustomer(@RequestParam String name, @RequestParam String email) {
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("customers")
+                .push();
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", name);
+        data.put("email", email);
+        ref.setValueAsync(data);
+        return "OK";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
